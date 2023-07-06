20a809c67fb1b0a5cc29625e383427c65acf7275
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2023-07-06 17:31:50 -0600

    merge twig count with nutrition data

--- Scripts/twig_proportion_merge.R
+++ Scripts/twig_proportion_merge.R
@@ -13,6 +13,9 @@ camfiles <- list.files(path = "Input/Camera_traps/", full.names = TRUE)
 cams <- lapply(camfiles, fread)
 cams <- rbindlist(cams, use.names = TRUE)
 
+cams[, Date := mdy(Date)]
+cams[Location == "KL 48", Date := Date + 365]
+
 camflags <- fread("Input/camera_flag_count.csv")
 
 
@@ -31,3 +34,51 @@ twigs[, pinkprop := `3_pink`/pink]
 
 #subset camera trap data to just proportions and info 
 twigs <- twigs[, .(Location, Date, snowdepth, Temp, grid, loc, orangeprop, yellowprop, pinkprop)]
+
+#rename flag colors to be height classes
+setnames(twigs, c("orangeprop", "yellowprop", "pinkprop"), c("low", "medium", "high"))
+
+#melt twig data
+twigs <- melt(twigs, measure.vars = c("low", "medium", "high"), variable.name = "height", value.name = "propavail")
+
+
+
+# get average biomass and nutrient biomass by grid for willow ------------------------
+
+#calculate available CP and Biomass
+nuts[, CPavail := Biomass*(CP/100)]
+nuts[, NDFavail := Biomass*(NDF/100)]
+
+#calculate total availability by height class, species, and grid
+totals <- nuts[, .(mean(Biomass), mean(CPavail), mean(NDFavail)), by = .(Species, Height, Grid)]
+names(totals) <- c("species", "height", "grid", "biomasstotal", "CPtotal", "NDFtotal")
+
+#take just willow from nutrient availability
+willow <- totals[species == "willow"]
+
+
+
+# merge starting nutrient avail with twig avail ---------------------------
+
+#merge total availability with proportions of twig available 
+willowavail <- merge(twigs, willow, by = c("grid","height"), all.x = TRUE)
+
+#now calc the actual availability of biomass, CP, and NDF with snow depth
+willowavail[, biomassavail := biomasstotal*propavail]
+willowavail[, CPavail := CPtotal*propavail]
+willowavail[, NDFavail := NDFtotal*propavail]
+
+ggplot(willowavail)+
+  geom_path(aes(x = Date, y = biomassavail, group = height, color = height))+
+  facet_wrap(~grid, scales = "free")
+
+
+# sum all willow avail across height by day --------------------------------------
+
+willowall <- willowavail[, .(mean(snowdepth), mean(Temp), sum(biomassavail), sum(CPavail), sum(NDFavail)), by = .(grid, Date)]
+names(willowall) <- c("grid", "date", "snowd", "temp", "biomass", "CP", "NDF")
+
+
+ggplot(willowall)+
+  geom_path(aes(x = date, y = biomass, group = grid, color = grid))
+
