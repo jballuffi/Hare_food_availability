4ce2ef20935da9aa5aae7cfe723e0594ee2c9674
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2023-07-07 11:42:31 -0600

    remove biomass merging and save nutrition stats instead

--- Scripts/03-starting_nutrition.R
+++ Scripts/03-starting_nutrition.R
@@ -3,21 +3,11 @@
 library(data.table)
 library(ggplot2)
 
-sums <- readRDS("Output/Data/transect_biomass.rds")
 nuts <- fread("Input/Plant_nutrition.csv")
 
 
 
-# prep biomass data -------------------------------------------------------
-
-#create data frame called heights. This shows one column with three options for height class
-sums[, total := NULL]
-heights <- melt(sums, measure.vars = c("low", "med", "high"), variable.name = "Height", value.name = "Biomass")
-heights[Height == "med", Height := "medium"]
-
-
-
-# prep nutrition data for merge -------------------------------------------
+# prep variable names for future merge -------------------------------------------
 
 #create species column with sample id
 nuts[grep("S", Sample), Species := "spruce"]
@@ -30,12 +20,6 @@ nuts[grep("H", Sample), Height := "high"]
 
 #remove random value with no speices or height
 nuts <- nuts[!is.na(Species)]
-
-
-#create data table of mean nutritional values by species and height
-meannuts <- nuts[, .(mean(NDF_F, na.rm = TRUE), mean(ADF_F, na.rm = TRUE), 
-                     mean(ADL_F, na.rm = TRUE), mean(CP_F, na.rm = TRUE)), by = .(Species, Height)]
-names(meannuts) <- c("Species", "Height", "NDF", "ADF", "ADL", "CP")
 
 
 
@@ -53,19 +37,31 @@ justnuts[, Nutrient := gsub("_F", "", Nutrient)]
 
 #figure to look at difference between height classes if any
 (allnuts <- 
-  ggplot(justnuts)+
-  geom_boxplot(aes(x = Species, y = Composition, fill = Height))+
-  labs(y = "Composition (%)", x = "Forage type")+
-  theme_minimal()+
-  facet_wrap(~ Nutrient, scales = "free"))
+    ggplot(justnuts)+
+    geom_boxplot(aes(x = Species, y = Composition, fill = Height))+
+    labs(y = "Composition (%)", x = "Forage type")+
+    theme_minimal()+
+    facet_wrap(~ Nutrient, scales = "free"))
 
 
 
-# merge nutrients and starting biomass ------------------------------------
+# create a summary datatable for export -----------------------------------
 
-#initial merge
-full <- merge(heights, meannuts, by = c("Species", "Height"), all.x = TRUE)
+# this will be merged with biomass and availability later
 
-  
-saveRDS(full, "Output/Data/nutrient_biomass.rds")
+#create data table of means, medians, and standard deviations for each nutritional value by species and height
+meannuts <- nuts[, .(mean(NDF_F, na.rm = TRUE), mean(ADF_F, na.rm = TRUE), mean(ADL_F, na.rm = TRUE), mean(CP_F, na.rm = TRUE), 
+                     median(NDF_F, na.rm = TRUE), median(ADF_F, na.rm = TRUE), median(ADL_F, na.rm = TRUE), median(CP_F, na.rm = TRUE),
+                     sd(NDF_F, na.rm = TRUE), sd(ADF_F, na.rm = TRUE), sd(ADL_F, na.rm = TRUE), sd(CP_F, na.rm = TRUE)), 
+                 by = .(Species, Height)]
+
+names(meannuts) <- c("species", "height", "NDF_mean", "ADF_mean", "ADL_mean", "CP_mean",
+                     "NDF_median", "ADF_median", "ADL_median", "CP_median",
+                     "NDF_sd", "ADF_sd", "ADL_sd", "CP_sd")
+
+
+
+# save outputs ------------------------------------------------------------
+
+saveRDS(meannuts, "Output/Data/starting_nutrition.rds")
 ggsave("Output/Figures/composition_by_height.jpeg", allnuts, width = 8, height = 6, unit = "in")
