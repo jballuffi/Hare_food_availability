2b504105803778587d3226ddc1c0d5fca05ae1d2
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2023-07-07 16:58:47 -0600

    merge all biomass, nutrition, and twig counts into one food sheet

---
+++ Scripts/food_merge.R
@@ -0,0 +1,62 @@
+
+library(data.table)
+library(lubridate)
+library(ggplot2)
+
+
+
+# read in all cleaned and prepped data ------------------------------------
+
+biomass <- readRDS("Output/Data/starting_biomass.rds")
+nuts <- readRDS("Output/Data/starting_nutrition.rds")
+willowgrid <- readRDS("Output/Data/starting_willow_avail_bygrid.rds")
+willowsite <- readRDS("Output/Data/starting_willow_avail_bysite.rds")
+
+
+
+
+# For now we are just merging together willow -----------------------------
+
+biomass <- biomass[species == "willow"][, species := NULL]
+nuts <- nuts[species == "willow"][, species := NULL]
+
+#### WHERE IS CAMERA LOCATION. PROBABLY NEED TO CALCULATE WITH THIS FIRST?
+
+# get total available biomass by day and grid -------------------------------------
+
+#cases where proportion available was greater than one
+willowgrid[propavail > 1, propavail := 1]
+
+#merge starting grid biomass data with twig count data
+willow <- merge(willowgrid, biomass, by = c("grid", "height"), all.x = TRUE)
+
+#calculate available willow biomass
+willow[, biomassavail := biomass_mean*propavail]
+
+
+
+# get avg composition of willow available ---------------------------
+
+#sum all biomass available across all three heights by day
+willow[, biomassavail_allheight := sum(biomassavail), by = .(grid, date)]
+
+#use the total biomass available to calculate the proportion of biomass each height contributes any day
+willow[, biomassprop := biomassavail/biomassavail_allheight]
+
+#merge in nutrition data
+willow <- merge(willow, nuts, by = "height", all.x = TRUE)
+
+#multiply composition by proportion available
+willow[, CPxbiomassprop := biomassprop*CP_mean]
+willow[, NDFxbiomassprop := biomassprop*NDF_mean]
+
+#sum biomass across all heights by day and calculate average compositions
+
+foodavail <- willow[, .(mean(temp), mean(snowdepth), sum(biomassavail), sum(CPxbiomassprop), sum(NDFxbiomassprop)), by = .(grid, date)]
+names(foodavail) <- c("grid", "date", "temp", "snowdepth", "biomassavail", "CPavail", "NDFavail")
+
+
+ggplot(foodavail)+
+  geom_path(aes(x = date, y = biomassavail, color = grid, group = grid))
+
+
