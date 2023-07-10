803111e24ed053051f0249ab8c2182fb24516aee
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2023-07-10 17:02:30 -0600

    predict DMD for plant samples, change figures to just willow for now

--- Scripts/03-starting_nutrition.R
+++ Scripts/03-starting_nutrition.R
@@ -1,9 +1,74 @@
+
 # script for translating biomass and nutritional compositions into total nutrients 
 
 library(data.table)
 library(ggplot2)
+library(ggeffects)
 
+#these are the results for plant samples taken before winter 2022-2023
 nuts <- fread("Input/Plant_nutrition.csv")
+
+#results from feeding trials
+days <- readRDS("../NutritionalGeometryHares/Output/data/dailyresultscleaned.rds")
+trials <- readRDS("../NutritionalGeometryHares/Output/data/trialresultscleaned.rds")
+
+
+# show trend for weight change an DMD -------------------------------------
+
+#linear model for diet DMD predicting weight change
+weightmod <- lm(Weight_change ~ DMD, trials)
+summary(weightmod)
+wint <- coef(weightmod)["(Intercept)"]
+wsl <- coef(weightmod)["DMD"]
+
+#show relationship between DMD and weight change 
+ggplot(trials, aes(x = DMD, y = Weight_change))+
+  geom_abline(intercept = 0, slope = 0, linetype = 2, color = "grey65", size = 1)+
+  geom_point()+
+  geom_abline(intercept = wint, slope = wsl, color = "grey20", size = 1)+
+  theme_minimal()
+
+
+
+# Predict digestibility from food composition in feeding trials  ----------
+
+#remove problematic response for now
+days <- days[! Sample == "E15_23-02-05"]
+
+#create CP:NDF ratio
+days[, CP_NDF := CP_diet/NDF_diet]
+
+#cut to just diet A and B
+daysAB <- days[Diet == "A" | Diet == "B"]
+
+#linear regression between protein:ndf ratio for just diet A and B 
+digmodAB <- lm(DMD ~ NDF_diet, daysAB)
+summary(digmodAB)
+dint <- coef(digmodAB)["(Intercept)"]   # get intercept
+dsl <- coef(digmodAB)["NDF_diet"]         # get slope
+
+#relationship between CP:NDF and DMD for just A and B
+ggplot(daysAB, aes(x = NDF_diet, y = DMD))+
+  geom_point()+
+  geom_abline(intercept = dint, slope = dsl)+
+  labs(x = "Proportion NDF", y = "Proportion DMD")+
+  theme_minimal()
+
+
+
+# additional polynomial regression for predicting DMD with all diets----------------
+
+#polynomial regression between protein:NDF and DMD
+digmod <- lm(DMD ~ poly(CP_NDF, 2), days)
+summary(digmod)
+digeffs <- ggpoly(digmod, terms = "CP_NDF")
+
+#plot relationship between CP:NDF and DMD, for all diets
+ggplot(days, aes(x = CP_NDF, y = DMD))+
+  geom_point()+
+  geom_smooth(method="lm", formula = y ~ poly(x, 2))+
+  labs(x = "CP:NDF", y = "DMD (%)")+
+  theme_minimal()
 
 
 
@@ -22,12 +87,21 @@ nuts[grep("H", Sample), Height := "high"]
 nuts <- nuts[!is.na(Species)]
 
 
+# Using feeding trial digestibility regression, estimate DMD --------------
+
+#calculate CP:NDF ratios
+nuts[, CP_NDF := CP_F/NDF_F]
+
+#use CP:NDF ratio to predict DMD
+nuts[, DMD := dint + (dsl*NDF_F/100)]
+
+
 
 # Investigate nutrition trends by height class ----------------------------
 
 #create a melted version of nutrient data, melted by nutrient
-justnuts <- nuts[, .(NDF_F, ADF_F, ADL_F, CP_F, Species, Height, Grid)]
-justnuts <- melt(justnuts, measure.vars = c("NDF_F", "ADF_F", "ADL_F", "CP_F"), variable.name = "Nutrient", value.name = "Composition")
+justnuts <- nuts[, .(NDF_F, ADF_F, ADL_F, CP_F, DMD, Species, Height, Grid)]
+justnuts <- melt(justnuts, measure.vars = c("NDF_F", "ADF_F", "ADL_F", "CP_F", "DMD"), variable.name = "Nutrient", value.name = "Composition")
 
 #make height class a leveled factor
 justnuts[, Height := factor(Height, levels = c("low", "medium", "high"), ordered = TRUE)]
@@ -35,15 +109,22 @@ justnuts[, Height := factor(Height, levels = c("low", "medium", "high"), ordered
 #rename nutrients for figures
 justnuts[, Nutrient := gsub("_F", "", Nutrient)]
 
-#figure to look at difference between height classes if any
+#figure to look at difference between height classes if any, switched plot to just willow for preliminary stuff
 (allnuts <- 
-    ggplot(justnuts)+
-    geom_boxplot(aes(x = Species, y = Composition, fill = Height))+
-    labs(y = "Composition (%)", x = "Forage type")+
+    ggplot(justnuts[!Nutrient == "DMD" & Species == "willow"])+
+    geom_boxplot(aes(x = Height, y = Composition))+
+    labs(y = "Composition (%)", x = "Browse height")+
     theme_minimal()+
     facet_wrap(~ Nutrient, scales = "free"))
 
+(justDMD <- 
+  ggplot(justnuts[Nutrient == "DMD" & Species == "willow"])+
+  geom_boxplot(aes(x = Height, y = Composition))+
+  labs(y = "Proportion DMD", x = "Browse height")+
+  theme_minimal())
 
+summary(lm(Composition ~ Height, data = justnuts[Nutrient == "DMD" & Species == "willow"]))
+  
 
 # create a summary datatable for export -----------------------------------
 
@@ -65,3 +146,6 @@ names(meannuts) <- c("species", "height", "NDF_mean", "ADF_mean", "ADL_mean", "C
 
 saveRDS(meannuts, "Output/Data/starting_nutrition.rds")
 ggsave("Output/Figures/composition_by_height.jpeg", allnuts, width = 8, height = 6, unit = "in")
+ggsave("Output/Figures/DMD_by_height.jpeg", justDMD, width = 6, height = 5, unit = "in")
+
+
