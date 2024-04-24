ccaf5ed0a0cab74330867139a91291bb317e5a7e
Juliana Balluffi-Fry <jballuffi@gmail.com>
2024-04-23 18:52:31 -0600

    remove grid from fecal protein

--- Scripts/fecal_protein.R
+++ Scripts/fecal_protein.R
@@ -9,12 +9,13 @@ fec <- fread("Input/fecal_CP.csv")
 #fill in missing DMs
 meandm <- fec[, mean(DM, na.rm = TRUE)]
 fec[is.na(DM), DM := meandm]
-fec[is.na(CP_F), CP_F := CP_AI/DM]
+fec[, CP_F := CP_AI/DM]
 
 #take grid and date from sample column
 fec[, date := tstrsplit(Sample, " ", keep = 2)]
 fec[, loc := tstrsplit(Sample, " ", keep = 1)]
 fec[, date := dmy(date)]
+fec[, m := as.factor(month(date))]
 
 fec[loc == "BT" | loc == "JO", grid := "Jo"]
 fec[loc == "KL" | loc == "KW", grid := "Kl"]
@@ -23,11 +24,14 @@ fec[loc == "AG", grid := "AG"]
 
 
 fecals <- 
-  ggplot(fec[!grid == "AG"])+
-  geom_point(aes(x = date, y = CP_F, color = grid))+
-  geom_smooth(aes(x = date, y = CP_F, group = grid, color = grid), alpha = .3)+
+  ggplot(fec)+
+  geom_point(aes(x = date, y = CP_F))+
+  geom_smooth(aes(x = date, y = CP_F), alpha = .3)+
   labs(y = "Crude protein (%)", x = "Date")+
   theme_minimal()
+
+ggplot(fec)+
+  geom_boxplot(aes(x = m, y = CP_F))
 
 
 ggsave("Output/Figures/fecal_protein.jpeg", fecals, width = 6, height = 4, unit = "in")
