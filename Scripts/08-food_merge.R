56d2f71019364026ce0d054b6faee91b57e2abdc
Juliana Balluffi-Fry <jballuffi@gmail.com>
2024-10-08 13:00:29 -0600

    remove some unnecessary things

--- Scripts/08-food_merge.R
+++ Scripts/08-food_merge.R
@@ -10,6 +10,9 @@ startingbiomass <- readRDS("Output/Data/starting_biomass.rds")
 startingnuts <- readRDS("Output/Data/starting_nutrition.rds")
 prop <- readRDS("Output/Data/proportion_available.rds")
 
+#change col names in starting nutrients
+setnames(startingnuts, c("Species", "Height"), c("species", "height"))
+
 
 
 # melt the proportion available by species --------------------------------
@@ -28,13 +31,12 @@ prop[, species := tstrsplit(species, "_", keep = 2)]
 
 #merge in biomass and proportion available
 #the median biomass is zero for spruce at lower height classes. Probably should use means
-biomass <- merge(prop, startingbiomass, by = c("grid", "species", "height"), all.x = TRUE)
-
-#change col names in starting nutrients
-setnames(startingnuts, c("Species", "Height"), c("species", "height"))
+biomass <- merge(prop, startingbiomass[, .(species, height, grid, biomass_mean)], 
+                 by = c("grid", "species", "height"), all.x = TRUE)
 
 #merge in quality to biomass and proportion available
-daily <- merge(biomass, startingnuts, by = c("species", "height"), all.x = TRUE)
+daily <- merge(biomass, startingnuts[, .(species, height, CP_mean)], 
+               by = c("species", "height"), all.x = TRUE)
 
 #reorder daily values
 daily <- daily[order(Location, Date, species)]
@@ -51,7 +53,6 @@ daily <- daily[!height == "allheights"]
 
 #calculate available biomass based on mean
 daily[, biomassavail := biomass_mean*propavail]
-daily[, biomassavail_sd := biomass_sd*propavail]
 
 
 
@@ -60,21 +61,18 @@ daily[, biomassavail_sd := biomass_sd*propavail]
 #sum all biomass available across all three heights by day
 daily[, biomassavail_all := sum(biomassavail), by = .(grid, loc, Date, species)]
 
-#use the total biomass available to calculate the proportion of biomass each height contributes any day
+#calculate the proportion of biomass each height contributes each day
 daily[, biomassprop := biomassavail/biomassavail_all]
 
 #multiply composition by proportion available
-daily[, CPxbiomassprop := biomassprop*CP_mean]
+daily[, CPxbiomassprop := biomassprop*CP_mean]  # I think this is wrong
 
-#sum biomass across all heights by day and calculate average compositions
-foodavail <- daily[, .(mean(temp), mean(Snow), mean(Moon), sum(biomassavail), sum(CPxbiomassprop)), by = .(grid, species, Date)]
-names(foodavail) <- c("grid", "species", "date", "temp", "snowdepth", "moon", "biomassavail", "CPavail")
 
 
 
 # save  -------------------------------------------------------------------
 
 #why is there a grid that is NA
-foodavail <- foodavail[!is.na(grid)]
+#foodavail <- foodavail[!is.na(grid)]
 
 saveRDS(foodavail, "Output/Data/Total_daily_food_availability.rds")
