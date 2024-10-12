1c9428b6134b472da0a74a49157e426ca3ac1094
Juliana Balluffi-Fry <jballuffi@gmail.com>
2024-10-12 16:29:52 -0600

    remove all heights

--- Scripts/08-food_merge.R
+++ Scripts/08-food_merge.R
@@ -25,8 +25,6 @@ prop <- melt.data.table(prop, measure.vars = c("propavail_willow", "propavail_sp
 #take only species name from species column
 prop[, species := tstrsplit(species, "_", keep = 2)]
 
-#remove all heights from prop avail
-prop <- prop[!height == "allheights"]
 
 
 # merge in biomass and quality --------------------------------------------------------
