e1a940f9d5be7bff8a10475ea943cf5e3739d0f7
Juliana Balluffi-Fry <jballuffi@gmail.com>
2024-10-14 14:01:03 -0600

    get mod names into lapply output, remove old working code

--- Scripts/06-predict_willow.R
+++ Scripts/06-predict_willow.R
@@ -23,44 +23,19 @@ highgam <- gam(propavail_willow ~ s(Snow), data = willow[height == "high"])
 summary(highgam)
 
 mods <- list(lowgam, medgam, highgam)
+modnames <- c("low", "medium", "high")
+
+modoutlist <- lapply(mods, getgam)
+
+modout <- rbindlist(modoutlist, idcol = "height")
+modout[, height := factor(height, labels = modnames)]
 
 
-modout <- rbindlist(lapply(mods, getgam))
 
-
-# Predict willow availabilitybased on GAM  --------------------------------------------
-
-#make data set of snow in cm
-newdatlow <- data.table(Snow = seq(1, 150), height = "low")
-newdatmed <- data.table(Snow = seq(1, 150), height = "medium")
-newdathigh <- data.table(Snow = seq(1, 150), height = "high")
-
-#predictions using the gam outputs and new data
-predlow <- predict(lowgam, newdatlow, type = 'response', se.fit = TRUE) 
-predmed <- predict(medgam, newdatmed, type = 'response', se.fit = TRUE) 
-predhigh <- predict(highgam, newdathigh, type = 'response', se.fit = TRUE) 
-
-#fill in new data with predicted value and lower/upper confidence limited
-newdatlow[, pred := predlow$fit]
-newdatlow[, lower := predlow$fit - 1.96 * predlow$se.fit]
-newdatlow[, upper := predlow$fit + 1.96 * predlow$se.fit]
-
-newdatmed[, pred := predmed$fit]
-newdatmed[, lower := predmed$fit - 1.96 * predmed$se.fit]
-newdatmed[, upper := predmed$fit + 1.96 * predmed$se.fit]
-
-newdathigh[, pred := predhigh$fit]
-newdathigh[, lower := predhigh$fit - 1.96 * predhigh$se.fit]
-newdathigh[, upper := predhigh$fit + 1.96 * predhigh$se.fit]
-
-#rbind all heights of data
-newdat <- rbind(newdatlow, newdatmed, newdathigh)
-newdat[, height := factor(height, levels = c("low", "medium", "high"))]
-
-
+# figures -----------------------------------------------------------------
 
 (willow_pred <-
-  ggplot(newdat)+
+  ggplot(modout)+
   geom_ribbon(aes(x = Snow, ymin = lower, ymax = upper), alpha = 0.5, fill = "grey70")+
   geom_path(aes(x = Snow, y = pred), color = "blue4", linewidth = .75)+
   labs(x = "Snow depth (cm)", y = "Predicted twig availablity")+
